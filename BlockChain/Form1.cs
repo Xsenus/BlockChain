0a2cd6914395bfbd3a544a69d6c311f296f8415a
Xsenus_Home <ilel@list.ru>
2019-06-17 14:06:06 +0700

    Lesson 1 (last)

--- BlockChain/Form1.cs
+++ BlockChain/Form1.cs
@@ -16,5 +16,28 @@ namespace BlockChain
         {
             InitializeComponent();
         }
+
+        private Chain _chain = new Chain();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (var block in _chain.Blocks)
+            {
+                listBox1.Items.Add(block);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            _chain.Add(textBox1.Text, "Admin");
+
+            foreach (var block in _chain.Blocks)
+            {
+                listBox1.Items.Add(block);
+            }
+
+        }
     }
 }
